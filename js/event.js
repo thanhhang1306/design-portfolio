b5db82f56fdf9bfed0e05018eb47b4815cb173cf
Hang Pham <hangtpham04@gmail.com>
2023-01-24 17:15:30 -0600

    feat(js): add index.js and event.js

---
+++ js/event.js
@@ -0,0 +1,7 @@
+$(document).addEventListener("DOMContentLoaded", function (event) {
+   masonryBuild();
+});
+$(document).addEventListener("DOMContentLoaded", function (event) {
+      navbarToggleSidebar();
+      navActivePage();
+   });
