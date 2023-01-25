9c5b337fa92239f67ab1cfb55969b4343f19b495
Hang Pham <hangtpham04@gmail.com>
2023-01-24 20:54:14 -0600

    feat(app.js): successfully collect form data from post

--- app.js
+++ app.js
@@ -2,9 +2,36 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000; 
+const https = require('https');
+// const { allowedNodeEnvironmentFlags } = require('process');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.listen(port, () => {
    console.log(`Newsletter app listening at http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/index.html', (req, res) => {
+   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/signup.html', (req, res) => {
+   res.sendFile(__dirname + '/signup.html');
+});
+
+app.get('/project.html', (req, res) => {
+   res.sendFile(__dirname + '/project.html');
+});
+
+app.post('/signup.html', (req, res) => {
+   var name = req.body.name;
+   var email = req.body.email;
+   var message = req.body.message;
+
+   console.log(`Name: ${name} + Email: ${email} + Message: ${message}`);
 });
